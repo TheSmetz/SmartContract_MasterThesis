35de27559fa45e744f49e0b5365702502f59b2c5
William <william.paciaroni97@gmail.com>
2021-02-01 16:00:23 +0100

    Intermediate Node

---
+++ intermediate_node.py
@@ -0,0 +1,12 @@
+class IntermediateNode:
+
+    #Calculates the off-chain code.
+    #'window' is its local window w_i[t]
+    def __init__(self, id):
+        self.ID = id
+
+    def offChainCode(self, window):
+        print(window)
+
+    def proofOfComputation(self, window):
+        self.offChainCode(window)
---
+++ main.py
@@ -0,0 +1,5 @@
+from intermediate_node import IntermediateNode
+
+i1 = IntermediateNode(3)
+
+i1.proofOfComputation("ciao")
