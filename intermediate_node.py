c29835543fe649516378d2c67fbde9345e5d48cd
TheSmetz <matteo.guerrini@studenti.unicam.it>
2021-02-01 16:06:15 +0100

    Update intermediate_node.py

--- intermediate_node.py
+++ intermediate_node.py
@@ -1,6 +1,6 @@
 class IntermediateNode:
 
-    #Calculates the off-chain code.
+    #Calculates the off-chain code .
     #'window' is its local window w_i[t]
     def __init__(self, id):
         self.ID = id
@@ -10,3 +10,4 @@ class IntermediateNode:
 
     def proofOfComputation(self, window):
         self.offChainCode(window)
+
