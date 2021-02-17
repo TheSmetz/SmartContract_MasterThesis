3b1c578280e920e97a343f613d919d66f325fb7b
William <william.paciaroni97@gmail.com>
2021-02-17 15:25:39 +0100

    ECC

--- Java/Diversity/src/App.java
+++ Java/Diversity/src/App.java
@@ -1,10 +1,19 @@
+import java.security.Security;
+
 import classes.Server;
+import encrypt.ECC;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Server s = new Server(1111);
-        s.setClient(5555, "10.0.2.208");
-        s.runServer();
+        // Server s = new Server(5555);
+        // s.setClient(5555, "10.0.2.208");
+        // s.runServer();
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        ECC.generateKeyPair();
+        byte[] signature = ECC.encrypt("CIAO");
+        System.out.println(signature);
+        boolean res = ECC.verify(ECC.getPublicKey(), signature, "CIAO");
+        System.out.println(res);
     }
 }
---
+++ Java/Diversity/src/encrypt/ECC.java
@@ -0,0 +1,61 @@
+package encrypt;
+
+import java.security.*;
+import java.security.spec.*;
+
+public class ECC {
+
+	private static PrivateKey privateKey;
+	private static PublicKey publicKey;
+
+	public ECC() {
+	}
+
+	public static void generateKeyPair() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			// Initialize the key generator and generate a KeyPair
+			keyGen.initialize(ecSpec, random); // 256 bytes provides an acceptable security level
+			KeyPair keyPair = keyGen.generateKeyPair();
+			// Set the public and private keys from the keyPair
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static PublicKey getPublicKey() {
+		return publicKey;
+	}
+
+	public static byte[] encrypt(String input) {
+		Signature dsa;
+		byte[] output = new byte[0];
+		try {
+			dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+	}
+
+	public static boolean verify(PublicKey pubKey, byte[] signature, String data) {
+		try {
+			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+			ecdsaVerify.initVerify(pubKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
