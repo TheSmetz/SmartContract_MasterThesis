c62cc2e4b1930daa5ece0b24bb8fefa4335049ad
Smetz <matteo.guerrini@studenti.unicam.it>
2021-02-18 15:45:58 +0100

    Update ACMessage.java

--- Java/Diversity/src/classes/ACMessage.java
+++ Java/Diversity/src/classes/ACMessage.java
@@ -1,5 +1,18 @@
 package classes;
 
 public class ACMessage implements MessageContent{
-    
+    public int t;      //time
+    public Integer[] w;
+    public String nounce; // S(t,i)
+
+
+    public ACMessage(int t, Integer[] w) {
+        this.t = t;
+        this.w = w;
+    }
+
+    public String generateACMessage(){
+        //TODO
+    }
+
 }
