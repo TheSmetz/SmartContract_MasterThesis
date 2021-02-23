9c406ab15dc5a2f78fef54d1c0a138598f50ef96
William <william.paciaroni97@gmail.com>
2021-02-23 15:24:50 +0100

    before maven

--- Java/Diversity/src/Trigger.java
+++ Java/Diversity/src/Trigger.java
@@ -1,25 +1,39 @@
 import java.lang.reflect.Type;
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.reflect.TypeToken;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import classes.*;
 import encrypt.ECC;
 import encrypt.JSONConverter;
 
+
+
 public class Trigger {
     public static void main(String[] args) throws Exception {
 
-        Security.addProvider(new BouncyCastleProvider());
-        ECC.generateKeyPair();
+        // Security.addProvider(new BouncyCastleProvider());
+        // ECC.generateKeyPair();
 
-        Client c = new Client(4444, "127.0.0.1");
-        c.runClient();
+        // Client c = new Client(4444, "127.0.0.1");
+        // c.runClient();
+        // InitMessage initMessage = new InitMessage();
+        // Type msgType = new TypeToken<Message<InitMessage>>() {}.getType();
+        // Message<InitMessage> m = new Message<InitMessage>(MessageType.INIT, initMessage);
+
+        // String msg = JSONConverter.toJSON(m, msgType);
+        // c.sendMessage(msg);
+
         InitMessage initMessage = new InitMessage();
         Type msgType = new TypeToken<Message<InitMessage>>() {}.getType();
         Message<InitMessage> m = new Message<InitMessage>(MessageType.INIT, initMessage);
 
-        String msg = JSONConverter.toJSON(m, msgType);
-        c.sendMessage(msg);
+        MessagePack m2 = new MessagePack();
+
+        
+        
 
     }
 }
